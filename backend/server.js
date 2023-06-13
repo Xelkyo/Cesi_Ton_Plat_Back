3dbc819c34717708a26594a82794f097880c3c86
Yshtar <aurian.lordonennode@viacesi.fr>
2023-06-13 14:09:35 +0200

    bdd

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,23 @@ const port = process.env.PORT || 5000
 
 // Initialisation d'Express
 const app = express()
+
+
+//Connexion à MongoDB
+const connectDB = require('./config/db')
+//Initialisation de Mongo
+connectDB()
+
+
+// Accepter les données envoyées par formulaire
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+// Routes
+app.use('/user', require('./routes/userRoute'))
+
+
 app.listen(port, () => {
 console.log(`Server started on ${port}`)
 })
