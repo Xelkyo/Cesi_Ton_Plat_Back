61a932243102b4ca23ca37724f0aea4748ad8d04
Yshtar <aurian.lordonennode@viacesi.fr>
2023-06-13 14:09:21 +0200

    bdd

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,3 +6,17 @@ const {
     userLogin,
     getUser
 } = require('../controllers/userController')
+
+router.post('/register', userRegister)
+router.post('/login', userLogin)
+router.get('/get', getUser)
+
+
+
+
+// Lancement du serveur
+app.listen(port, () => {
+    console.log(`Server started on ${port}`)
+})
+
+
