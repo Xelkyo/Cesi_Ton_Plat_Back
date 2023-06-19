1cf056963505f94ddf180891d71640f9f9e4cdf5
Yshtar <aurian.lordonennode@viacesi.fr>
2023-06-19 14:58:59 +0200

    Fix userMiddleware

--- api-gateway/middlewares/userMiddlerware.js
+++ api-gateway/middlewares/userMiddlerware.js
@@ -1,15 +1,17 @@
 const registry = require('../routes/registry.json')
 
-const nameUrl = registry.services[req.params.apiName].url
+const deliver = (req, res, next, requestOption) => {
+    const nameUrl = registry.services[req.params.apiName].url
 
-fetch(nameUrl + req.params.path, requestOption)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        res.send(data.msg)
-    })
-    .catch((error) => {
-        console.error(error)
-        res.status(500).send('Internal Server Error')
-    })
+    fetch(nameUrl + req.params.path, requestOption)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            res.send(data.msg)
+        })
+        .catch((error) => {
+            console.error(error)
+            res.status(500).send('Internal Server Error')
+        })
+}
