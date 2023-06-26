cdf7808fe0197caff5ee5cbfde350e2f03dbd677
mtramine <amine.mehtar@hotmail.com>
2023-06-26 13:47:41 +0200

    Add getRestaurantById

--- user/routes/authRoutes.js
+++ user/routes/authRoutes.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController')
 
 router.post('/login', userController.userLogin)
 router.post('/register', userController.userRegister)
-router.get('/restaurants', userController.getRestaurants) // accessed by all
+router.get('/restaurants', userController.getRestaurants) // accessed by customers only
+router.get('/restaurant/:id', userController.getRestaurantById)
+
 module.exports = router
