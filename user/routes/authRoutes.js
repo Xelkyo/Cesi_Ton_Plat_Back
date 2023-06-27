62ee4d0ee60412e6e13d7ead2cc6461f6762e340
mtramine <amine.mehtar@hotmail.com>
2023-06-27 11:28:18 +0200

    Move restaurants routes from auth to user

--- user/routes/authRoutes.js
+++ user/routes/authRoutes.js
@@ -4,9 +4,5 @@ const userController = require('../controllers/userController')
 
 router.post('/login', userController.userLogin)
 router.post('/register', userController.userRegister)
-router.get('/restaurants', userController.getRestaurants) // accessed by customers only
-router.get('/restaurant/:id', userController.getRestaurantById)
-router.post('/restaurant', userController.createRestaurant)
-router.get('/restaurants/managers/:id', userController.getRestaurantsByManagerId)
 
 module.exports = router
