e84c01f691ee1819cf580ac5587604749f02fa89
mtramine <amine.mehtar@hotmail.com>
2023-06-27 14:27:15 +0200

    Add order functions

--- order/controllers/orderController.js
+++ order/controllers/orderController.js
@@ -1,9 +1,18 @@
-const Order = require('../models/order');
+const Order = require('../models/Order');
 
 const createOrder = async (req, res) => {
     try {
         const order = await Order.create(req.body);
         res.status(200).json({ order });
+    } catch (err) {
+        res.status(400).json({ msg: err });
+    }
+}
+
+const getOrders = async (req, res) => {
+    try {
+        const orders = await Order.find();
+        res.status(200).json({ orders });
     } catch (err) {
         res.status(400).json({ msg: err });
     }
@@ -36,4 +45,14 @@ const updateOrder = async (req, res) => {
     }
 }
 
-module.exports = { createOrder };
+// get orders of logged user
+const getOrdersByUser = async (req, res) => {
+    try {
+        const orders = await Order.find({ user: req.params.id });
+        res.status(200).json({ orders });
+    } catch (err) {
+        res.status(400).json({ msg: err });
+    }
+}
+
+module.exports = { createOrder, getOrderById, updateOrder, getOrdersByUser, getOrders };
--- order/routes/orderRoutes.js
+++ order/routes/orderRoutes.js
@@ -5,5 +5,7 @@ const orderController = require('../controllers/orderController')
 router.get('order/:id', orderController.getOrderById)
 router.post('order', orderController.createOrder)
 router.put('order/:id', orderController.updateOrder)
+router.get('order/user/:id', orderController.getOrdersByUser)
+router.get('order', orderController.getOrders)
 
 module.exports = router
